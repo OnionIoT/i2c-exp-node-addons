5b29a3bb02ca7470387b083849e7981f783c09d7
ratatatatat <rajiv.puri1192@gmail.com>
2016-05-21 13:56:07 -0400

    -Removed the checkInit()

--- Examples/pwm_node_example.js
+++ Examples/pwm_node_example.js
@@ -2,10 +2,10 @@ var pwmAddon = require("/usr/bin/pwm-exp-addon");
 
 //Wait 3 Seconds,Check initialization, Should return 0 if PWM Expansion has just been plugged in
 
-setTimeout(function(){
-console.log("Checking if PWM Expansion is initialized");
-console.log("checkInit:",pwmAddon.checkInit());
-},3000);
+//setTimeout(function(){
+//console.log("Checking if PWM Expansion is initialized");
+//console.log("checkInit:",pwmAddon.checkInit());
+//},3000);
 
 
 //Wait 3 Seconds, initialize the expansion
@@ -15,10 +15,11 @@ console.log("driverInit:",pwmAddon.driverInit());
 },6000);
 
 //Wait 3 seconds, check initialization, should return 1 since the expansion was just initializaed above. 
-setTimeout(function(){
-console.log("Checking if PWM Expansion is initialized");
-console.log("checkInit:",pwmAddon.checkInit());
-},9000);
+//setTimeout(function(){
+//console.log("Checking if PWM Expansion is initialized");
+//console.log("checkInit:",pwmAddon.checkInit());
+//},9000);
+
 //Wait 3 seconds, set channel 0 
 setTimeout(function(){
 console.log("Setting Channel 0 t0 50% Duty Cycle");
