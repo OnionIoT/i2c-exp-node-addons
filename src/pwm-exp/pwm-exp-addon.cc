7126daa7e8b073e35609bf16b779179fb2f10be9
James Liu <liujames.q@gmail.com>
2017-02-14 18:08:17 -0500

    all argument checking implemented for addons, compiles error-free

--- src/pwm-exp/pwm-exp-addon.cc
+++ src/pwm-exp/pwm-exp-addon.cc
@@ -1,4 +1,4 @@
-// relayexpaddon.cc
+// pwm-exp-addon.cc
 #include <node.h>
 #include <v8.h>
 #include <pwm-exp.h>
@@ -20,6 +20,12 @@ using v8::String;
 using v8::Value;
 
 
+// NodeJS wrapper for C pwmCheckInit() Function
+//  arguments:
+//    callback - function (optional)
+//  returns:
+//    true - PWM expansion has started
+//    false - expansion has not started
 void checkInit(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
 
@@ -29,10 +35,10 @@ void checkInit(const FunctionCallbackInfo<Value>& args) {
     Local<Number> retVal = Number::New(isolate, bInit);
 
     // handle (optional) callback argument
-    if (args.Length() > 1) {
+    if (args.Length() > 0) {
         Local<Function> callback = Local<Function>::Cast(args[0]);
         const unsigned argc = 1;
-        Local<Value> argv[argc] = { Number::New(isolate, bInit) };
+        Local<Value> argv[argc] = { retVal };
         callback->Call(Null(isolate), argc, argv);
     }
 
@@ -40,6 +46,12 @@ void checkInit(const FunctionCallbackInfo<Value>& args) {
 }
 
 
+// NodeJS wrapper for C pwmDriverInit() Function
+//  arguments:
+//    callback - function (optional)
+//  returns:
+//    true - PWM initialisation successful
+//    false - initialisation failed
 void driverInit(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
 
@@ -58,6 +70,12 @@ void driverInit(const FunctionCallbackInfo<Value>& args) {
 }
 
 
+// NodeJS wrapper for C pwmDisableChip() Function
+//  arguments:
+//    callback - function (optional)
+//  returns:
+//    true - PWM successfully disabled
+//    false - PWM unable to be disabled
 void disableChip(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
 
@@ -76,8 +94,33 @@ void disableChip(const FunctionCallbackInfo<Value>& args) {
     args.GetReturnValue().Set(retVal);
 }
 
+
+// NodeJS wrapper for C pwmSetFrequency() Function
+//  arguments:
+//    frequency - double - the desired frequency to be set
+//    callback - function (optional)
+//  returns:
+//    true - relay state is on
+//    false - relay is off
 void setFrequency(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
+
+    // Check the number of arguments passed.
+    if (args.Length() < 1) {
+        // Throw an Error that is passed back to JavaScript
+        isolate->ThrowException(Exception::TypeError(
+            String::NewFromUtf8(isolate, "Wrong number of arguments")));
+        return;
+    }
+
+    // Check the argument types
+    if (!args[0]->IsNumber()) {
+        isolate->ThrowException(Exception::TypeError(
+            String::NewFromUtf8(isolate, "Wrong argument type")));
+        return;
+    }
+
+    // read in the value arguments
     double frequency = args[0]->NumberValue();
 
     // perform the C library operation
@@ -95,8 +138,38 @@ void setFrequency(const FunctionCallbackInfo<Value>& args) {
     args.GetReturnValue().Set(retVal);
 
 }
+
+
+// NodeJS wrapper for C pwmSetupDriver() Function
+//  arguments:
+//    channel - integer - specifies which a single channel to driver
+//    duty - double - the duty cycle in percent to set the channel to
+//    delay - double - the delay in percent in a cycle before the output raises to HIGH
+//    callback - function (optional)
+//  returns:
+//    true - relay state is on
+//    false - relay is off
 void setupDriver(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
+
+    // Check the number of arguments passed.
+    if (args.Length() < 2) {
+        // Throw an Error that is passed back to JavaScript
+        isolate->ThrowException(Exception::TypeError(
+            String::NewFromUtf8(isolate, "Wrong number of arguments")));
+        return;
+    }
+
+    // Check the argument types
+    if (!args[0]->IsNumber() ||
+        !args[1]->IsNumber() ||
+        !args[2]->IsNumber()) {
+        isolate->ThrowException(Exception::TypeError(
+            String::NewFromUtf8(isolate, "Wrong argument type")));
+        return;
+    }
+
+    // read in the value arguments
     int channel = args[0]->IntegerValue();
     double duty = args[1]->NumberValue();
     double delay = args[2]->NumberValue();
@@ -114,7 +187,6 @@ void setupDriver(const FunctionCallbackInfo<Value>& args) {
     }
 
     args.GetReturnValue().Set(retVal);
-
 }
 
 void init(Local<Object> exports) {
